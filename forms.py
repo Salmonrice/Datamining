96efd54f0b049c4b83c4adcdd23e319490bf03f3
Salmon.witch <salmon.witch@gmail.com>
2020-11-11 18:20:45 +0700

    finish model

--- forms.py
+++
@@ -1,8 +0,0 @@
-from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
-from wtforms.validators import DataRequired
-
-class InputForm(FlaskForm):
-    username = StringField('Username', validators=[DataRequired()])
-    password = StringField('Pass', validators=[DataRequired()])
-    submit = SubmitField('Submit')
